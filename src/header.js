4af7310221db12be07b6dc1a6a3e6bf31f2b773e
BUNNY_SCHAEFER <SCHAEFERBUNNY@GMAIL.COM>
2021-05-10 14:00:38 -0700

    added dependencies and created first component

---
+++ src/header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    // ...
+    return <h1>What to watch</h1>
+
+  }
+}
+
+export default Header; // Don’t forget to use export default!
