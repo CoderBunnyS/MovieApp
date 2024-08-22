179b304d1e501d0739105a7dcbfc39166f3b4070
BUNNY_SCHAEFER <SCHAEFERBUNNY@GMAIL.COM>
2024-08-21 17:54:14 -0700

    updated search bar to allow for user to press enter

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,48 +1,56 @@
-import React, { useState } from 'react'
-import '../Assets/Search.css'
-
-//create search bar & drop down menu
-//create click function
-
+import React, { useState } from 'react';
+import '../Assets/Search.css';
 
 const SearchBar = ({ fetchCall }) => {
-    const [userSearch, setuserSearch] = useState('')
-    const [typeChoice, setTypeChoice] = useState('')
+  const [userSearch, setUserSearch] = useState('');
+  const [typeChoice, setTypeChoice] = useState('');
 
+  const handleInput = (e) => {
+    e.preventDefault();
+    setUserSearch(e.target.value);
+  };
 
-    const handleInput = (e) => {
-        e.preventDefault()
-        //console.log(e)
-        setuserSearch(e.target.value)
+  const handleClick = (e) => {
+    e.preventDefault();
+    fetchCall(userSearch, typeChoice);
+  };
+
+  const handleTypeChoice = (e) => {
+    e.preventDefault();
+    setTypeChoice(e.target.value);
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      handleClick(e);
     }
+  };
 
-    const handleClick = (e) => {
-        e.preventDefault()
-        //console.log(e)
-        fetchCall(userSearch, typeChoice)
-    }
-
-    const handleTypeChoice = (e) => {
-        e.preventDefault()
-        //console.log(e)
-        setTypeChoice(e.target.value)
-    }
-
-    return (
-        <div className='container-header'>
-            <h1 className='headerText'>What to watch</h1>
-            <div>
-                <input onChange={handleInput} type='text' placeholder='Title' />
-                <select id="selection" name="selection" placeholder='Type' onChange={handleTypeChoice}>
-                    <option value="" hidden>Type</option>
-                    <option value="movie">Movies</option>
-                    <option value="series">Series</option>
-                    <option value="episode">Episode</option>
-                </select>
-                <button onClick={handleClick}>Search</button>
-            </div>
-        </div>
-    );
+  return (
+    <div className='container-header'>
+      <h1 className='headerText'>What to watch</h1>
+      <div>
+        <input
+          onChange={handleInput}
+          onKeyPress={handleKeyPress}
+          type='text'
+          placeholder='Title'
+        />
+        <select
+          id="selection"
+          name="selection"
+          placeholder='Type'
+          onChange={handleTypeChoice}
+        >
+          <option value="" hidden>Type</option>
+          <option value="movie">Movies</option>
+          <option value="series">Series</option>
+          <option value="episode">Episode</option>
+        </select>
+        <button onClick={handleClick}>Search</button>
+      </div>
+    </div>
+  );
 };
 
-export default SearchBar
+export default SearchBar;
